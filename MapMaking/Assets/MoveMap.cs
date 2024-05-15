e4729f0d91d6d88589fea22488269417a6ac496c
배찬호 <1004chanho@gmail.com>
2024-05-15 20:24:57 +0900

    맵 이동 구현
    맵이름만 맞춰두면 그 맵에 맞게 카메라랑 캐릭터 이동 구현

--- MapMaking/Assets/MoveMap.cs
+++ MapMaking/Assets/MoveMap.cs
@@ -8,31 +8,35 @@ public class MoveMap : MonoBehaviour
 {
     // Start is called before the first frame update
     public string transferMapName; //이동할 맵이름       
-    public Transform target; // 이동할 타겟 설정
+    
 
     private Playercontroller thePlayer; 
     private Camera theCamera;
-    public GameObject mapcenter;
+    private GameObject cameraTarget;// 맵이동 카메라 타겟 설정
+    private GameObject target; // 맵이동 플레이어 타겟 설정
 
     // Start is called before the first frame update
     void Start()
     {
-        thePlayer = FindObjectOfType<Playercontroller>();
-        theCamera = FindObjectOfType<Camera>();
-        
+        thePlayer = FindObjectOfType<Playercontroller>();//플레이어 객체 가져옴
+        theCamera = FindObjectOfType<Camera>();//카메라 가져옴
     }
 
-    // 박스 콜라이더에 닿는 순간 이벤트 발생
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player") //플레이어가 콜라이더 닿으면
         {
-            thePlayer.currentMapName = transferMapName;
-            mapcenter = GameObject.Find(transferMapName);
-            print(transferMapName + "으로 이동");
-            theCamera.transform.position = new Vector3(mapcenter.transform.position.x, theCamera.transform.position.y, theCamera.transform.position.z);
-            thePlayer.transform.position = target.transform.position;
+            thePlayer.currentMapName = transferMapName; //플레이어가 이동할 맵이름 설정
+            cameraTarget = GameObject.Find(transferMapName);//카메라가 이동할 타겟 값설정
+
+            //플레이어가 이동할 위치(문의 좌표)가져옴
+            target = GameObject.Find(transferMapName+"/Door");//가져온 맵이름이랑 같은 오브젝트 자식중 Door를 찾아서 가져옴
+
+            print(transferMapName + "으로 이동");//확인용 
+            theCamera.transform.position = new Vector3(cameraTarget.transform.position.x, cameraTarget.transform.position.y, theCamera.transform.position.z);//카메라 이동
+            thePlayer.transform.position = target.transform.position;//가져온 스폰위치에다 캐릭터 이동
 
         }
     }
