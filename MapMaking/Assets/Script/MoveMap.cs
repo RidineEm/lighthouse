25def53373d7fa75c15fe0f5c4ef1f52c8796175
배찬호 <1004chanho@gmail.com>
2024-05-20 00:15:56 +0900

    ㅅㅂ 팔레트 잘못 건드렸다 작업한거 날려먹음
    문 오브젝트 이름변경하고 기능만 복구해놓음 ㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂㅅㅂ

--- MapMaking/Assets/Script/MoveMap.cs
+++ MapMaking/Assets/Script/MoveMap.cs
@@ -26,14 +26,14 @@ public class MoveMap : MonoBehaviour
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        isclear = GameObject.Find("isclear").GetComponent<RoomManager>().isclear;//클리어 상태 확인용으로 가져옴
+        isclear = GameObject.Find("RoomManager").GetComponent<RoomManager>().isclear;//클리어 상태 확인용으로 가져옴
         if (collision.gameObject.name == "Player"&&isclear==true) //플레이어가 콜라이더 닿고,클리어상태일때
         {
             thePlayer.currentMapName = transferMapName; //플레이어가 이동할 맵이름 설정
             cameraTarget = GameObject.Find(transferMapName);//카메라가 이동할 타겟 값설정
             
             //플레이어가 이동할 위치(문의 좌표)가져옴
-            target = GameObject.Find(transferMapName+"/Door");//가져온 맵이름이랑 같은 오브젝트 자식중 Door를 찾아서 가져옴
+            target = GameObject.Find(transferMapName+"/Entrance");//가져온 맵이름이랑 같은 오브젝트 자식중 Entrance를 찾아서 가져옴
 
             print(transferMapName + "으로 이동");//확인용 
             theCamera.transform.position = new Vector3(cameraTarget.transform.position.x, cameraTarget.transform.position.y, theCamera.transform.position.z);//카메라 이동
